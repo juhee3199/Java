6aeaab3c37b5ec0c9b3846356a155484a4fc55a1
juhee3199 <43562691+juhee3199@users.noreply.github.com>
2019-07-24 22:59:49 +0900

    Add files via upload

---
+++ chap9/src/Ex_9_4/ButtonEx.java
@@ -0,0 +1,15 @@
+package Ex_9_4;
+
+public class ButtonEx {
+
+	public static void main(String[] args) {
+		Button btn = new Button();
+
+		btn.setOnClickListener(new CallListener());
+		btn.touch(); 
+		
+		btn.setOnClickListener(new MessageListener());
+		btn.touch();
+	}
+
+}
---
+++ chap9/src/Ex_9_4/CallListener.java
@@ -0,0 +1,7 @@
+package Ex_9_4;
+
+public class CallListener implements Button.OnClickListener {
+	public void onClick() {
+		System.out.println("전화를 겁니다.");
+	}
+}
